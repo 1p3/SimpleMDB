0ef5a3eedbfd48b0f95f081f12590cf93a732a80
Yadiel <yadielcondediaz@gmail.com>
2025-12-11 21:31:13 -0400

    Add Movies API Server App

--- src/Smdb.Api/Movies/MoviesRouter.cs
+++ src/Smdb.Api/Movies/MoviesRouter.cs
@@ -1,6 +1,7 @@
 namespace Smdb.Api.Movies;
 
 using Shared.Http;
+
 public class MoviesRouter : HttpRouter
 {
     public MoviesRouter(MoviesController moviesController)
@@ -9,8 +10,7 @@ public class MoviesRouter : HttpRouter
         MapGet("/", moviesController.ReadMovies);
         MapPost("/", HttpUtils.ReadRequestBodyAsText, moviesController.CreateMovie);
         MapGet("/:id", moviesController.ReadMovie);
-        MapPut("/:id", HttpUtils.ReadRequestBodyAsText,
-         moviesController.UpdateMovie);
+        MapPut("/:id", HttpUtils.ReadRequestBodyAsText, moviesController.UpdateMovie);
         MapDelete("/:id", moviesController.DeleteMovie);
     }
 }
